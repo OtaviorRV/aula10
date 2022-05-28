65012afcdb57f4cb9631fee3e86573ff3bc76c66
OtaviorRV <71786857+OtaviorRV@users.noreply.github.com>
2022-05-27 21:50:57 -0300

    Add files via upload

--- App.js
+++ App.js
@@ -1,11 +1,11 @@
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
-import  {ScreenA }from "./src/screens/screenA/index";
-import { ScreenB } from './src/screens/screenB';
-
+// import  {ScreenA }from "./src/screens/screenA/index";
+// import { ScreenB } from './src/screens/screenB';
+import { Routes } from './src/routes';
 export default function App() {
   return (
-    <ScreenA/>
+    <Routes />
     
     
   );
